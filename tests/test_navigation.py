1c680cb792c809d36ce764364c2fcb73756c96d9
Ru Cindrea <ext-ru.cindrea@vaisala.com>
2022-02-16 08:26:56 +0200

    starting point with POM for appium session

--- tests/test_navigation.py
+++ tests/test_navigation.py
@@ -18,40 +18,4 @@ class TestNavigation:
         my_plants_page = MyPlantsPage(self.driver)
         plant_page = PlantPage(self.driver)
         
-        
-        welcome_page.check_all_elements()
-        welcome_page.click_start()
-        assert user_page.is_displayed()
-        
-        user_page.check_all_elements()
-        user_page.enter_user_name("Ru Cindrea")
-        user_page.click_confirm()
-        assert confirmation_page.is_displayed()
-        
-        confirmation_page.check_all_elements()
-        confirmation_page.confirm()
-        assert add_plant_page.is_displayed()
-        
-        add_plant_page.check_all_elements()
-        add_plant_page.click_my_plants_tab()
-        assert my_plants_page.is_displayed()
-        
-        my_plants_page.check_all_elements()
-        my_plants_page.click_add_plant_tab()
-        assert add_plant_page.is_displayed()
-        
-        add_plant_page.select_plant_by_id(0)
-        assert plant_page.is_displayed()
-        
-        plant_page.check_all_elements()
-        plant_page.cancel_plant()
-        assert add_plant_page.is_displayed()
-        
-        add_plant_page.select_plant_by_id(0)
-        plant_page.save_plant()
-        assert confirmation_page.is_displayed()
-        
-        confirmation_page.check_all_elements()
-        confirmation_page.confirm()
-        assert add_plant_page.is_displayed()
-        
+        pass
