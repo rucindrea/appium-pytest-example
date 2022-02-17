a4648bb7f2ea3401a8f4c19d98558df7427a5ce4
Ru Cindrea <ext-ru.cindrea@vaisala.com>
2022-02-17 13:25:11 +0200

    remove allure descriptions from initial test

--- tests/test_first.py
+++ tests/test_first.py
@@ -4,11 +4,8 @@ import pytest
 from appium import webdriver
 from appium.webdriver.common.appiumby import AppiumBy
 
-
-@allure.description("First test for Plant Manager App")
 class TestPlantManagerApp:
     driver: webdriver
     
-    @allure.title("Test all the things")
     def test_first_screen(self):
         pass
